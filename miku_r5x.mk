c63804e10e4f49bf88477034d4e4eda90f9b71e0
futaro180 <nafiulhaque357@gmail.com>
2025-03-26 10:47:40 +0600

    Update miku_r5x.mk

--- miku_r5x.mk
+++ miku_r5x.mk
@@ -1,5 +1,3 @@
-
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
