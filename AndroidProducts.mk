c5f85e0c549a3df345df47872bae94faa8e2469f
futaro180 <nafiulhaque357@gmail.com>
2025-03-26 10:48:24 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,2 @@
-#
-# Copyright (C) 2022-2024 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/miku_r5x.mk
-
-COMMON_LUNCH_CHOICES := \
-     miku_r5x-user \
-     miku_r5x-userdebug \
-     miku_r5x-eng
