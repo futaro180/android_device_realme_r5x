1be4c75529bc4ef4f361075110825281ed0e3542
futaro180 <nafiulhaque357@gmail.com>
2025-03-26 10:47:02 +0600

    Update and rename lineage_r5x.mk to miku_r5x.mk

--- miku_r5x.mk
+++ miku_r5x.mk
@@ -1,15 +1,11 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AxionAOSP stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common miku stuff
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -17,13 +13,10 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_r5x
+PRODUCT_NAME := miku_r5x
 PRODUCT_MODEL := realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-# AxionAOSP stuff
-AXION_CAMERA_REAR_INFO := 12,8,2,2
-AXION_CAMERA_FRONT_INFO := 8
-AXION_MAINTAINER := Amrito_Karmokar
-AXION_PROCESSOR := Snapdragon_665™
+# Maintainer
+MIKU_MASTER := Proximus
